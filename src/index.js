afe66efa044dfeb3b69bbdde940906b5e5322546
DianaE23 <123725630+DianaE23@users.noreply.github.com>
2023-07-01 11:21:08 +0000

    commit index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    Link,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
 } from "react-router-dom";
 
 import Home from './components/home/home';
 import About from './components/about/about';
-import App from './app.js';
-import './styles.css';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element:<Home/>,
-    },
+{
+    path: "/",
+    element:<Home/>,
+},
 
-    {
-        path: "about",
-        element:<About/>,
-    },
-
+{
+    path: "about",
+    element:<About/>,
+},
 ]);
 
-createRoot(<App/>,document.getElementById("root")).render(
-    <RouterProvider router={router} />
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
